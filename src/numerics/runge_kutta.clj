4d0349b1d5f5c7bd21b7deae68283d5b3fbd48a1
Mark Bastian <markbastian@gmail.com>
2015-02-27 00:07:09 -0700

    rk4 for systems works.

--- src/numerics/runge_kutta.clj
+++ src/numerics/runge_kutta.clj
@@ -10,25 +10,67 @@
 (defn dot [u v] (reduce + (map * u v)))
 
 (defn ks[f ic h { :keys [a c]}]
-  (loop [k [] ai a ci c]
+  (let [fty (apply juxt f)]
+    (loop [k [] ai a ci c]
     (if (= (count c) (count k))
       k
-      (let [deltas [(* h (first ci)) (* h (dot (first ai) k))]
-            fc (map + ic deltas)]
-        (recur (conj k (f fc)) (rest ai) (rest ci))))))
+      (let [[tn & yn] ic
+            t (+ tn (* (first ci) h))
+            y (map + yn (map (fn [k] (* h (reduce + (map #(* % k) (first ai))))) (or (peek k) (repeat 0))))
+            ty (into [t] y)]
+        (recur (conj k (fty ty)) (rest ai) (rest ci)))))))
 
 ;Note - for adaptive RK, just do two deltas inside of here
 (defn rk-step [f ic h { b :b :as tableau }]
   (let [k (ks f ic h tableau)
-        deltas [h (* h (dot b k))]]
-    (vec (map + ic deltas))))
+        deltas (map dot (repeat b) (apply map vector k))
+        [tn & yn] ic]
+    (into [(+ tn h)] (map + yn (map #(* % h) deltas)))))
 
 (def ic [0 4 6])
 (defn f [[_ y1 _]] (* -0.5 y1))
 (defn g [[_ y1 y2]] (- 4.0 (* 0.3 y2) (* 0.1 y1)))
 
-(prn (f ic))
-(prn (g ic))
+(prn (ks [f g] ic 0.5 rk4-tableau))
+(prn (rk-step [f g] ic 0.5 rk4-tableau))
 
-(prn (f [0.25 3.5 6.45]))
-(prn (g [0.25 3.5 6.45]))
+;(def c [0 0.5 0.5 1.0])
+;(def a [[]
+;        [0.5]
+;        [0.0 0.5]
+;        [0.0 0.0 1.0]])
+;(def b [(/ 1.0 6.0) (/ 1.0 3.0) (/ 1.0 3.0) (/ 1.0 6.0)])
+;
+;(def h 0.5)
+;(def ic [0 4 6])
+;(defn f [[_ y1 _]] (* -0.5 y1))
+;(defn g [[_ y1 y2]] (- 4.0 (* 0.3 y2) (* 0.1 y1)))
+;(def fg (juxt f g))
+;
+;(def k0 (let [[tn & yn] ic
+;              t (+ tn (* (c 0) h))
+;              y (map + yn (map (fn [k] (* h (reduce + (map #(* % k) (a 0))))) [0 0]))]
+;          (fg (into [t] y))))
+;
+;(prn k0)
+;
+;(def k1 (let [[tn & yn] ic
+;              t (+ tn (* (c 1) h))
+;              y (map + yn (map (fn [k] (* h (reduce + (map #(* % k) (a 1))))) k0))]
+;          (fg (into [t] y))))
+;
+;(prn k1)
+;
+;(def k2 (let [[tn & yn] ic
+;              t (+ tn (* (c 2) h))
+;              y (map + yn (map (fn [k] (* h (reduce + (map #(* % k) (a 2))))) k1))]
+;          (fg (into [t] y))))
+;
+;(prn k2)
+;
+;(def k3 (let [[tn & yn] ic
+;              t (+ tn (* (c 3) h))
+;              y (map + yn (map (fn [k] (* h (reduce + (map #(* % k) (a 3))))) k2))]
+;          (fg (into [t] y))))
+;
+;(prn k3)
