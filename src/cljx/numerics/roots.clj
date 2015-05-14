c24422c5a009e37d65f5657d366bc009d138014f
Mark Bastian <markbastian@gmail.com>
2015-05-13 20:43:57 -0600

    Latest changes.

--- src/cljx/numerics/roots.clj
+++ src/cljx/numerics/roots.clj
@@ -3,16 +3,16 @@
 (defn early-exit [xfx] (when (== 0 (second xfx)) xfx))
 
 (defn bracket [step-func f bounds tol]
-  (let [[l u :as lu] (map (juxt identity f) bounds)]
-    (or (early-exit l) (early-exit u)
+  (let [lu (map (juxt identity f) bounds)]
+    (or (some identity (map early-exit lu))
         (when (neg? (apply * (map second lu)))
-          (loop [[xl fxl :as l] l [xu _ :as u] u]
-            (let [[_ fxm :as m] (step-func f l u)]
+          (loop [[l u] lu]
+            (let [m (step-func f l u)]
               (cond
-                (== 0.0 fxm) m
-                (<= (Math/abs (- xl xu)) tol) (min-key #(-> % second Math/abs) l m u)
-                (neg? (* fxl fxm)) (recur l m)
-                :else (recur m u))))))))
+                (early-exit m) m
+                (<= (Math/abs (- (l 0) (u 0))) tol) (min-key #(-> % second Math/abs) l m u)
+                (neg? (* (l 1) (m 1))) (recur [l m])
+                :else (recur [m u]))))))))
 
 (defn midpoint [f [xl _] [xh _]] ((juxt identity f) (* 0.5 (+ xl xh))))
 (defn secant [f [xl fxl] [xh fxh]] ((juxt identity f) (- xh (/ (* fxh (- xh xl)) (- fxh fxl)))))
@@ -22,10 +22,13 @@
 
 (prn (bisect #(+ 0.1 (Math/sin %)) [-1 1] 1E-10))
 (prn (bisect #(Math/sin %) [0.4 1] 1E-10))
+(prn (bisect #(Math/sin %) [-1 1] 1E-10))
 (prn (bisect #(Math/sin %) [0 1] 1E-10))
 (prn (bisect #(Math/sin %) [0 0] 1E-10))
+(prn (bisect #(Math/sin %) [-9 0] 1E-10))
 (prn (false-pos #(Math/sin %) [-3 0] 1E-10))
 (prn (false-pos #(+ 0.1 (Math/sin %)) [-1 1] 1E-10))
+(prn (false-pos #(+ 0.1 (Math/tan %)) [-1 1] 1E-10))
 
 (defn secant-step
   ([f x dx] (secant-step [f [x (f x)] [(+ x dx) (f (+ x dx))]]))
